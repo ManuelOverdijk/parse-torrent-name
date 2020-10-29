5620447c44b8c57513d9a82bcb6825933c89c9eb
Manuel Overdijk <manuel.overdijk@gmail.com>
2020-10-29 12:19:47 +0100

    fix year regex to allow 2020-2030 dates

--- parts/common.js
+++ parts/common.js
@@ -9,7 +9,7 @@ var core = require('../core');
 var patterns = {
   season: /([Ss]?([0-9]{1,2}))[Eex]/,
   episode: /([Eex]([0-9]{2})(?:[^0-9]|$))/,
-  year: /([\[\(]?((?:19[0-9]|20[01])[0-9])[\]\)]?)/,
+  year: /([\[\(]?((?:19[0-9]|20[0123])[0-9])[\]\)]?)/,
   resolution: /(([0-9]{3,4}p))[^M]/,
   quality: /(?:PPV\.)?[HP]DTV|(?:HD)?CAM|B[rR]Rip|TS|(?:PPV )?WEB-?DL(?: DVDRip)?|H[dD]Rip|DVDRip|DVDRiP|DVDRIP|CamRip|W[EB]B[rR]ip|[Bb]lu[Rr]ay|DvDScr|hdtv/,
   codec: /xvid|x264|h\.?264/i,
